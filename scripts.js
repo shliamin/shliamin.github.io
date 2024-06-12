87ac1ece02bbbe98078afa2a39803e4af835037f
Efim <47456474+shliamin@users.noreply.github.com>
2024-06-12 06:08:30 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,17 @@
-function toggleSidebar() {
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+    document.getElementById(sectionId).classList.add('active');
+    
+    // Закрываем меню на мобильных устройствах после выбора секции
+    if (window.innerWidth <= 768) {
+        document.querySelector('.sidebar').classList.remove('active');
+    }
+}
+
+function toggleMenu() {
     document.querySelector('.sidebar').classList.toggle('active');
 }
 
