68e65b2ed9e29afd524909ceb69978384c8b542f
Efim <47456474+shliamin@users.noreply.github.com>
2024-06-12 07:21:40 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,19 @@
 function showSection(sectionId) {
     const sections = document.querySelectorAll('.section');
+    const buttons = document.querySelectorAll('.sidebar nav ul li button');
+
     sections.forEach(section => {
         section.classList.remove('active');
     });
-    document.getElementById(sectionId).classList.add('active'); 
 
-    if (window.innerWidth <= 768) {
-        document.querySelector('.sidebar').classList.remove('active');
-    }
+    buttons.forEach(button => {
+        button.classList.remove('active');
+    });
+
+    document.getElementById(sectionId).classList.add('active');
+    document.querySelector(`button[onclick="showSection('${sectionId}')"]`).classList.add('active');
 }
+
 
 function toggleMenu() {
     document.querySelector('.sidebar').classList.toggle('active');
