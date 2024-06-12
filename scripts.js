2f227d9026386b1cb70a289b12f14a7687d4e85c
Efim <47456474+shliamin@users.noreply.github.com>
2024-06-12 18:10:34 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -20,6 +20,4 @@ function toggleMenu() {
     document.querySelector('.sidebar').classList.toggle('active');
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    showSection('home');
-});
+
